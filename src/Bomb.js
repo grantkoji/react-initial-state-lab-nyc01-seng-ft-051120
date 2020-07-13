5bc7b52d5f8a66be20afe752af0dcf7dd1d9b154
grantkoji <grantkoji@gmail.com>
2020-07-12 23:49:35 -0400

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,27 @@
 // your Bomb code here!
+import React from 'react'
+
+class Bomb extends React.Component {
+    constructor(props) {
+        super()
+        this.state = {
+           secondsLeft: props.initialCount
+        }
+    }
+
+     
+
+    render () {
+        let message = 'You entered a negative time'
+        if (this.state.secondsLeft === 0) {
+            message = 'Boom!' 
+        } else if (this.state.secondsLeft > 0) {
+            message = `${this.state.secondsLeft} seconds left before I go boom!` 
+        }
+        return (
+            <div>{message}</div>
+          )
+    }
+}
+
+export default Bomb
